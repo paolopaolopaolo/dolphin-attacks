ccccde8ebfd16e864e61aea4ce7cc6c82564bf0e
Dean Mercado <dean@yeti.co>
2016-04-22 16:30:02 -0700

    Add staging environment

---
+++ config/settings/stage.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "+e[VQmMT__@UVB*DUgVM|TBUwhkpqOnJ?6D0%%R0"
+
+ALLOWED_HOSTS = ["107.170.31.120", ]
