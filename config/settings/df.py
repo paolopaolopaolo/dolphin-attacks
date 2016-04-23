25b6040093dbad0f6f8cffa5a0a6d9e983280fa0
Dean Mercado <dean@yeti.co>
2016-04-22 17:34:38 -0700

    Magic dolphin goodness
    - Add server
    - Fix no-newline issue
    - Add TD2 environment

---
+++ config/settings/df.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "RJi+uWM27A>QPk6<f%G5A=+5+k:gdgV!=ESg.W&M"
+
+ALLOWED_HOSTS = ["dolphin.dpmercado.com", ]
