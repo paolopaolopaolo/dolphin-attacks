58198a98d029d2a11c5af5f1532b8aef43c8565e
Dean Mercado <dean@yeti.co>
2016-04-25 10:17:15 -0700

    Add TD2 environment

---
+++ config/settings/td2.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "O[vv?*TAJzK[%zgvcpM|q8HC_9d&z2QIGakr-bBH"
+
+ALLOWED_HOSTS = ["162.243.26.202", ]
